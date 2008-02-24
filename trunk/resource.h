21abd0018bf5e323fcd177842f5d26b74a3cd902
jiangzhanyong <jiangzhanyong@25ef18b7-e546-0410-9864-e1898eb54b0c>
2008-02-24 11:54:51 +0000

    第一次导入到 Google Code。 正式版本号 V2.0.3.B2

---
+++ trunk/resource.h
@@ -0,0 +1,72 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CvtCNKI.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CVTCNKI_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDR_POPUPMENU                   129
+#define IDR_SPONORMENU1                 130
+#define IDR_SPONORMENU2                 131
+#define IDC_FILE                        1000
+#define IDC_CLIPBOARD                   1001
+#define IDC_REFERENCETYPE               1002
+#define IDC_CONVERT                     1003
+#define IDC_OUTPUTSTYLE                 1005
+#define IDC_DATABASENAME                1006
+#define IDC_ANNOTATION                  1007
+#define IDC_PAGES                       1008
+#define IDC_LANG                        1009
+#define IDC_OPENOUTPUTFILE              1011
+#define IDC_SETOUTPUTDIR                1012
+#define IDC_POPUPMENU                   1013
+#define IDC_POPUPMENU2                  1014
+#define IDC_OPENOUTPUTFILE2             1014
+#define IDC_WEBSITE                     1014
+#define IDC_ABOUT                       1016
+#define IDC_EDITINFO                    1017
+#define IDC_BUTTON1                     1022
+#define ID_LICENSE                      32779
+#define ID_PROJECTHOMEPAGE              32780
+#define ID_DOWNLOADHOMEPAGE             32781
+#define ID_SERVICESHOMEPAGE             32782
+#define ID_SPONSOR2                     32784
+#define ID_SPONSOR1                     32785
+#define ID_DBCFGUTF8                    32788
+#define ID_HELPME                       32789
+#define ID_32790                        32790
+#define ID_DBCFGOPENPLAN                32791
+#define ID_32792                        32792
+#define ID_NOTEEXPRESS_NOTEEXPRESS      32793
+#define ID_NOTEEXPRESS_WEBSITES         32794
+#define ID_NOTEEXPRESS_WEBSITE          32795
+#define ID_NOTEEXPRESS_NOTEEXPRESS32796 32796
+#define ID_NOTEEXPRESS_FORUM            32797
+#define ID_NOTEEXPRESS_NOTEEXPRESS32798 32798
+#define ID_NOTEEXPRESS_NOTEEXPRESS32799 32799
+#define ID_NOTEEXPRESS_NOTEEXPRESS32800 32800
+#define ID_NOTEEXPRESS_CMC              32801
+#define ID_NOTEEXPRESS_CTC              32802
+#define ID_NOTEEXPRESS_TUANGOU          32803
+#define ID_32804                        32804
+#define ID_32805                        32805
+#define ID_32806                        32806
+#define ID_32807                        32807
+#define ID_PAANGOOD_WEBSITE             32808
+#define ID_PAANGOOD_FORUM               32809
+#define ID_PAANGOOD_WEBLOG              32810
+#define ID_PAANGOOD_DAILI               32811
+#define ID_PAANGOOD_AGENCY              32812
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
